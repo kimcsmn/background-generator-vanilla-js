d922ec84e8dafd512be25714e17ee6e063299ef7
kimcsmn <kim.csmn@gmail.com>
2020-04-21 12:59:43 +0800

    added a function that will update the background-image text on the page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // GETTING ELEMENTS FROM DOM
 const color1 = document.querySelector('.color-inputs .color1');
 const color2 = document.querySelector('.color-inputs .color2');
-const cssCode = document.querySelector('header .code');
+const cssText = document.querySelector('header .code');
 
 // EVENT LISTENERS
 color1.addEventListener('change', changeBG);
@@ -11,4 +11,10 @@ color2.addEventListener('change', changeBG);
 function changeBG(event){
     // console.log(event.target.value);
     document.body.style = `background-image: linear-gradient(to right, ${color1.value}, ${color2.value});`;
+    updateBgText();
+}
+
+function updateBgText(){
+    const bg = document.body.style.backgroundImage;
+    cssText.innerText = 'background-image: ' + bg;
 }
