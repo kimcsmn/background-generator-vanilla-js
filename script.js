c24ab55180beaef1f39c730c2042749951f6247d
kimcsmn <kim.csmn@gmail.com>
2020-04-21 12:19:55 +0800

    change changeColorValues to changeBG

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // GETTING ELEMENTS FROM DOM
 const color1 = document.querySelector('.color-inputs .color1');
 const color2 = document.querySelector('.color-inputs .color2');
+const cssCode = document.querySelector('header .code');
 
 // EVENT LISTENERS
-color1.addEventListener('change', changeColorValues)
-color2.addEventListener('change', changeColorValues)
+color1.addEventListener('change', changeBG);
+color2.addEventListener('change', changeBG);
 
 // FUNCTIONS
-function changeColorValues(event){
+function changeBG(event){
     // console.log(event.target.value);
     document.body.style = `background-image: linear-gradient(to right, ${color1.value}, ${color2.value});`;
 }
-
