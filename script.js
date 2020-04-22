8e3fcf6b413cd04431b9590f1c155789f2e68f41
kimcsmn <kim.csmn@gmail.com>
2020-04-22 18:41:30 +0800

    got gradient shapes and gradient direction values

--- script.js
+++ script.js
@@ -2,16 +2,19 @@
 const color1 = document.querySelector('.color-inputs .color1');
 const color2 = document.querySelector('.color-inputs .color2');
 const cssText = document.querySelector('header .code');
+const gradient = document.querySelector('.gradient');
 
 // EVENT LISTENERS
 color1.addEventListener('input', changeBG);
 color2.addEventListener('input', changeBG);
+gradient.addEventListener('click', getGradient);
 
 // FUNCTIONS
 function changeBG(event){
     // console.log(event.target.value);
     document.body.style = `background-image: linear-gradient(to right, ${color1.value}, ${color2.value});`;
     updateBgText();
+
 }
 
 function updateBgText(){
@@ -19,3 +22,13 @@ function updateBgText(){
     cssText.innerText = 'background-image: ' + bg;
 }
 
+// to get the gradient element
+// add event listeren 
+// get the targets value first child and second childs value
+function setGradient(e){
+    console.log(e.target.parentElement.className);
+    const gShape = e.target.parentElement.className;
+    console.log(gDirection);
+    const gDirection = e.target.value;
+    // changeBG();
+};
