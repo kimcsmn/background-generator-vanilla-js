6eaa6cb9e630d96d698e058f288c51a9e3d1816a
kimcsmn <kim.csmn@gmail.com>
2020-04-22 19:25:42 +0800

    refactor: removed unecessary comments and console.logs

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const color1 = document.querySelector('.color-inputs .color1');
 const color2 = document.querySelector('.color-inputs .color2');
 const cssText = document.querySelector('header .code');
 const gradient = document.querySelector('.gradient');
-let body = document.body;
+const body = document.body;
 
 // EVENT LISTENERS
 color1.addEventListener('input', changeBG);
@@ -11,8 +11,7 @@ color2.addEventListener('input', changeBG);
 gradient.addEventListener('click', setGradient);
 
 // FUNCTIONS
-function changeBG(event){
-    // console.log(event.target.value);
+function changeBG(){
     document.body.style = `background-image: linear-gradient(to right, ${color1.value}, ${color2.value});`;
     updateBgText();
 
@@ -25,10 +24,7 @@ function updateBgText(){
 
 function setGradient(e){
     const gShape = e.target.parentElement.className;
-    console.log(e.target.parentElement.className);
     const gDirection = e.target.value;
-    console.log(gDirection);
-    console.log(body);
-    body.style= `background-image: ${gShape}-gradient(${gDirection}, ${color1.value}, ${color2.value});`;
+    body.style= `backgroun-image: ${gShape}-gradient(${gDirection}, ${color1.value}, ${color2.value});`;
     updateBgText();
 };
